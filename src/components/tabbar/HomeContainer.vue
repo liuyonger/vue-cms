84b73f6069f3f7a9d0a22f7c9898841fc68db035
liuyonger <1050511353@qq.com>
2019-03-11 20:50:43 +0800

    页面更新

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+
+
+
+        <!--轮播图区域-->
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+
+
+        <h3>HomeContainer</h3>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {}
+    },
+    created(){
+        this.getLunbotu();
+    },
+    methods: {
+        getLunbotu(){ 
+            //获取轮播图数据的方法
+            this.$http.get('http://vue.studyit.io/api/getlunbo').then(result => {
+                console.log(result.body);
+            });
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe{
+    height: 200px;
+
+    .mint-swipe-item {
+        &:nth-child(1){
+            background-color: red;
+        }
+        &:nth-child(2){
+            background-color: blue;
+        }
+        &:nth-child(3){
+            background-color: cyan;
+        }
+    }    
+    }
+</style>
+
+
